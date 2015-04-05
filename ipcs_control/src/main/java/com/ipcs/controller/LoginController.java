b3fcc107b2fad1600406ee341243edf222a3e4b5
seeyou8286 <seeyou8286@163.com>
2015-04-06 00:33:50 +0800

    Update datasource to make ipcs_control available. Why controller can not read the dataproperties file?

--- ipcs_control/src/main/java/com/ipcs/controller/LoginController.java
+++ ipcs_control/src/main/java/com/ipcs/controller/LoginController.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.ipcs.model.Person;
 import com.ipcs.service.SecurityService;
-import com.ipcs.service.impl.SecurityServiceImpl;
 
 @Controller
 public class LoginController {
