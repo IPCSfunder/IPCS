6c13e89b888ae7f9eb721379b6740d22aff4aad9
seeyou8286 <seeyou8286@163.com>
2015-04-10 00:46:35 +0800

    Update controller

--- ipcs_control/src/main/java/com/ipcs/controller/LoginController.java
+++ ipcs_control/src/main/java/com/ipcs/controller/LoginController.java
@@ -10,11 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 import com.ipcs.model.Person;
 import com.ipcs.service.SecurityService;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
     @Autowired
-    private SecurityService securityService;    
+    private SecurityService securityService;
 
     public SecurityService getSecurityService() {
         return securityService;
@@ -27,18 +29,16 @@ public class LoginController {
     @RequestMapping(value = "/userlogin", method = RequestMethod.GET)
     public ModelAndView student() {
 
-	return new ModelAndView("login", "command", new Person());
+        return new ModelAndView("login", "command", new Person());
     }
 
     @RequestMapping(value = "/adminLogin", method = RequestMethod.POST)
-    public String addStudent(@ModelAttribute Person admin, ModelMap model) {
-	boolean authenticated = securityService.authenticateLoginInfo(
-		admin.getAccount_name(), admin.getPassword_hash());
-	if (authenticated) {
-	    model.addAttribute("name", admin.getAccount_name());
-	    model.addAttribute("password", admin.getPassword_hash());
-	    return "navigator";
-	} else
-	    return "error";
+    public String addStudent(HttpSession request, @ModelAttribute Person admin, ModelMap model) {
+        boolean authenticated = securityService.authenticateLoginInfo(
+                admin.getAccount_name(), admin.getPassword_hash());
+        if (authenticated) {
+            return "navigator";
+        } else
+            return "error";
     }
 }
