50df4353893d2fe8b4a6f5e9424ebf2241b62602
seeyou8286 <seeyou8286@163.com>
2015-04-16 22:11:13 +0800

    Update datasouse and update table name, linux is case sensitive

--- ipcs_model/src/main/java/com/ipcs/model/Person.java
+++ ipcs_model/src/main/java/com/ipcs/model/Person.java
@@ -33,8 +33,8 @@ public class Person extends BasicObject {
 
     public Person() {
         super();
-        roles.add(new Role());
-        schools.add(new School());
+//        roles.add(new Role());
+//        schools.add(new School());
     }
 
 
