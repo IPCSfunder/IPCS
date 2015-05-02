d96e403dcf5d1952fa0ff6fd67437c0e815c4756
seeyou8286 <seeyou8286@163.com>
2015-05-02 20:40:33 +0800

    Add RelationshipTypeDao

---
+++ ipcs_domain/src/main/java/com/ipcs/dao/RelationshipTypeDao.java
@@ -0,0 +1,12 @@
+package com.ipcs.dao;
+
+import com.ipcs.model.Activity;
+import com.ipcs.model.RelationshipType;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Chen Chao
+ */
+@Repository
+public class RelationshipTypeDao extends GenericHibernateDao<RelationshipType, Long>{
+}
