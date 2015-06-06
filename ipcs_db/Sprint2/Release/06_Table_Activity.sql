f994bedcff94a01b26233c879be3473fa6987f3a
seeyou8286 <seeyou8286@163.com>
2015-06-07 00:18:43 +0800

    Update activity table

--- ipcs_db/Sprint2/Release/06_Table_Activity.sql
+++ ipcs_db/Sprint2/Release/06_Table_Activity.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS `ipcs`.`activity` (
   `school_fk` INT NULL,
   `activity_type_fk` INT NULL,
   `start_time` TIMESTAMP NULL,
+  `end_time` TIMESTAMP NULL,
   `date_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`activity_objid`))
